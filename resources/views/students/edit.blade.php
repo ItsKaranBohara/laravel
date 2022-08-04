45696c307cb1cef177383ca81c7c7a5666a06981
karan bohara <karanbohara420@gmail.com>
2022-08-04 15:09:47 +0545

    crud add update delete

---
+++ resources/views/students/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('students.layout')
+@section('content')
+ 
+<div class="card">
+  <div class="card-header">Contactus Page</div>
+  <div class="card-body">
+      
+      <form action="{{ url('student/' .$students->id) }}" method="post">
+        {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{$students->id}}" id="id" />
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" value="{{$students->name}}" class="form-control"></br>
+        <label>Address</label></br>
+        <input type="text" name="address" id="address" value="{{$students->address}}" class="form-control"></br>
+        <label>Mobile</label></br>
+        <input type="text" name="mobile" id="mobile" value="{{$students->mobile}}" class="form-control"></br>
+        <input type="submit" value="Update" class="btn btn-success"></br>
+    </form>
+   
+  </div>
+</div>
+ 
+@stop
---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,19 @@
+@extends('students.layout')
+@section('content')
+ 
+ 
+<div class="card">
+  <div class="card-header">Students Page</div>
+  <div class="card-body">
+   
+ 
+        <div class="card-body">
+        <h5 class="card-title">Name : {{ $students->name }}</h5>
+        <p class="card-text">Address : {{ $students->address }}</p>
+        <p class="card-text">Mobile : {{ $students->mobile }}</p>
+  </div>
+       
+    </hr>
+  
+  </div>
+</div>
